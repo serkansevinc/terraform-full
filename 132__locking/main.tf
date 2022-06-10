3bb928ae76bc21e7be79f21343fcce5f722c76ee
Serkan Sevinc <s-sevinc@outlook.com>
2022-06-10 12:07:27 +0200

    Lock and unlocking

--- 132__locking/main.tf
+++ 132__locking/main.tf
@@ -1,28 +1,52 @@
 terraform {
-  backend "remote" {
-    organization = "ExamPro"
+  cloud {
+    organization = "DaCompany"
 
     workspaces {
       name = "force-unlocking"
     }
   }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
 }
 
-provider "aws" {
-	profile = "default"
-	region = "us-east-1"
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
-module "apache" {
-	source  = "ExamProCo/apache-example/aws"
-  version = "1.1.0"
-	vpc_id = var.vpc_id
-	my_ip_with_cidr = var.my_ip_with_cidr
-	public_key = var.public_key
-	instance_type = var.instance_type
- 	server_name = var.server_name
+resource "azurerm_resource_group" "example" {
+  count    = 2
+  name     = "${var.rsgname}-${count.index}"
+  location = var.location
+  tags = {
+    Name = "Server-${count.index}"
+  }
 }
 
-output "public_ip" {
-  value = module.apache.public_ip
+variable "location" {
+  type        = string
+  description = "The location for deployment"
+  default     = "West Europe"
+}
+
+variable "rsgname" {
+  type        = string
+  description = "Resource Group name"
+  default     = "terraform-example"
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.example[*].id
+  # sensitive = true
 }
